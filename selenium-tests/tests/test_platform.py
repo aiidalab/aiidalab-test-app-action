e4b38d2fbb69ac5ebf92a78ec33fb5cbab9958d5
Simon Adorf <simon.adorf@epfl.ch>
2020-07-01 14:52:16 +0200

    Extend the basic platform tests to check for widgets view.
    To more reliably detect an error state.

--- selenium-tests/tests/test_platform.py
+++ selenium-tests/tests/test_platform.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 def test_login(selenium, url):
     selenium.get(url())
     selenium.find_element(By.ID, 'ipython-main-app')
+    selenium.find_element(By.ID, 'notebook-container')
+    selenium.find_element(By.CLASS_NAME, 'jupyter-widgets-view')
     selenium.get_screenshot_as_file('screenshots/login.png')
 
 
@@ -26,4 +28,6 @@ def test_apps(selenium, url):
 def test_apps_home(selenium, url):
     selenium.get(url('apps/apps/home/start.ipynb'))
     selenium.find_element(By.ID, 'ipython-main-app')
+    selenium.find_element(By.ID, 'notebook-container')
+    selenium.find_element(By.CLASS_NAME, 'jupyter-widgets-view')
     selenium.get_screenshot_as_file('screenshots/home.png')
